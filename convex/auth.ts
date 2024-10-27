eb0b8938648ac17c14a6b3526fcd378003d29e7a
Shivalal <shivalalprasad04@gmail.com>
2024-10-27 23:19:14 +0530

    github

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,7 @@
 import GitHub from '@auth/core/providers/github'
 // import Google from '@auth/core/providers/google'
 import { convexAuth } from '@convex-dev/auth/server'
-// import { Password } from '@convex-dev/auth/providers/Password'
+// import { Password } from '@convex-dev/auth/providers/Password '
 
 // import { DataModel } from './_generated/dataModel'
 
