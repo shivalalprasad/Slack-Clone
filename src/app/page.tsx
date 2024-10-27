c9de32dfe804bd7413d412d2ab9124f8892d7a01
Shivalal <shivalalprasad04@gmail.com>
2024-10-27 20:05:17 +0530

    Authentication UI ✔️ || signup or login page ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl bg-[#2f2f2f]">
-      Slack Clone
-    </div>
+    <AuthScreen/>
   );
 }
