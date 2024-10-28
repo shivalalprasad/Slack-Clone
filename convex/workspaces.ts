685d5df66ff0111ee738a7613cafc7e7090fffce
Shivalal <shivalalprasad04@gmail.com>
2024-10-28 19:24:51 +0530

    workspace members unique ✔️

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,6 +1,17 @@
 import { v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 import { auth } from './auth'
+
+const generateCode = () => {
+  const code = Array.from(
+    {
+      length: 6
+    },
+    () => '0123456789abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random() * 36)]
+  ).join('')
+  return code
+}
+
 export const create = mutation({
   args: {
     name: v.string()
@@ -11,19 +22,42 @@ export const create = mutation({
       throw new Error('Unauthorized')
     }
     // TODO: create a proper method later
-    const joinCode = '12456'
+    const joinCode = generateCode()
     const workspaceId = await ctx.db.insert('workspaces', {
       name: argv.name,
       userId,
       joinCode
     })
+
+    await ctx.db.insert('members', {
+      userId,
+      workspaceId,
+      role: 'admin'
+    })
+
     return workspaceId
   }
 })
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query('workspaces').collect()
+    const userId = await auth.getUserId(ctx)
+    if (!userId) {
+      return []
+    }
+    const members = await ctx.db
+      .query('members')
+      .withIndex('by_user_id', (q) => q.eq('userId', userId))
+      .collect()
+    const workspaceIds = members.map((member) => member.workspaceId)
+    const workspaces = []
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId)
+      if (workspace) {
+        workspaces.push(workspace)
+      }
+    }
+    return workspaces
   }
 })
 
@@ -34,6 +68,15 @@ export const getById = query({
     if (!userId) {
       throw new Error('Unauthorized')
     }
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspacec_id_user_id', (q) => q.eq('workspaceId', args.id).eq('userId', userId))
+      .unique()
+    if (!member) {
+      return null
+    }
+
+
     return await ctx.db.get(args.id)
   }
 })
