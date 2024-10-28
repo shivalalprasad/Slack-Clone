0b460b8b8f29e7cce77d7b9b720986dcfb5b8ce4
Shivalal <shivalalprasad04@gmail.com>
2024-10-28 11:27:08 +0530

    useer button component ✔️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 
 import { UserButton } from "@/features/auth/components/user-button";
 // import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+// import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
+  // const { signOut } = useAuthActions();
   return (
     <div className="h-full flex items-center justify-center bg-blue-800 text-white">
-      <p>
+      {/* <p>
       Logged in!
-      </p>
+      </p> */}
       <UserButton/>
       {/* <Button size='lg' variant='destructive' className="m-4" onClick={() => signOut()}>Sign Out</Button> */}
     </div>
