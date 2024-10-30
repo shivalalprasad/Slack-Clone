43381578d24a1752175c0faddca86550b5fc7bec
Shivalal <shivalalprasad04@gmail.com>
2024-10-30 19:56:42 +0530

    editor component ✔️ 1/2 ✅

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { Loader, TriangleAlert } from 'lucide-react'
 import { useGetChannel } from '@/features/channels/api/use-get-channel'
 import { useChannelId } from '@/hooks/use-channel-id'
 import Header from './header'
+import ChatInput from './chat-input'
 export default function ChannelIdPage() {
   const channelId = useChannelId()
   const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId })
@@ -24,6 +25,8 @@ export default function ChannelIdPage() {
   return (
     <div className='flex flex-col h-full'>
       <Header title={channel.name} />
+      <div className='flex-1' />
+      <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   )
 }
