c0670d45ff8e7eea2fce2503865a5538bc98f3a6
Shivalal <shivalalprasad04@gmail.com>
2024-10-31 09:57:42 +0530

    added image picker ✔️

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,7 +3,7 @@
 import { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { PiTextAa } from 'react-icons/pi'
 import { MdSend } from 'react-icons/md'
-import { ImageIcon, Smile } from 'lucide-react'
+import { ImageIcon, Smile, XIcon } from 'lucide-react'
 import Quill, { type QuillOptions } from 'quill'
 import { Delta, Op } from 'quill/core'
 import { cn } from '@/lib/utils'
@@ -11,6 +11,7 @@ import Hint from './hint'
 import { Button } from './ui/button'
 import { EmojiPopover } from './emoji-popover'
 import 'quill/dist/quill.snow.css'
+import Image from 'next/image'
 type EditorValue = {
   image: File | null
   body: string
@@ -34,6 +35,7 @@ export default function Editor({
   variant = 'create'
 }: EditorProps) {
   const [text, setText] = useState('')
+  const [image, setImage] = useState<File | null>(null)
   const [isToolbarVisible, setToolbarVisible] = useState(true)
   const submitRef = useRef(onSubmit)
   const placeholderRef = useRef(placeholder)
@@ -41,6 +43,7 @@ export default function Editor({
   const defaultValueRef = useRef(defaultValue)
   const containerRef = useRef<HTMLDivElement>(null)
   const disabledRef = useRef(disabled)
+  const imageElementRef = useRef<HTMLInputElement | null>(null)
   useLayoutEffect(() => {
     submitRef.current = onSubmit
     placeholderRef.current = placeholder
@@ -114,8 +117,38 @@ export default function Editor({
   const isEmpty = text.replace(/<(.|\n)*?>/g, '').trim().length === 0
   return (
     <div className='flex flex-col'>
+      <input
+        type='file'
+        accept='image/*'
+        ref={imageElementRef}
+        onChange={(e) => setImage(e.target.files![0])}
+        className='hidden'
+      />
       <div className='flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-md transition bg-white'>
         <div ref={containerRef} className='h-full ql-custom' />
+        {!!image && (
+          <div className='p-2'>
+            <div className='relative size-[62px] flex items-center justify-center group/image'>
+              <Hint label='Remove image'>
+                <button
+                  onClick={() => {
+                    setImage(null)
+                    imageElementRef.current!.value = ''
+                  }}
+                  className='hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-white items-center justify-center'
+                >
+                  <XIcon className='size-3.5' />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt='upload'
+                fill
+                className='rounded-xl overflow-hidden border object-cover'
+              />
+            </div>
+          </div>
+        )}
         <div className='flex px-2 pb-2 z-[5]'>
           <Hint label={isToolbarVisible ? 'Hide formatting' : 'Show formatting'}>
             <Button disabled={disabled} size='iconSm' variant='ghost' onClick={toggleToolbar}>
@@ -129,7 +162,7 @@ export default function Editor({
             </EmojiPopover>
           {variant === 'create' && (
             <Hint label='Attach file'>
-              <Button disabled={disabled} size='iconSm' variant='ghost' onClick={() => {}}>
+              <Button disabled={disabled} size='iconSm' variant='ghost' onClick={() => imageElementRef.current?.click()}>
                 <ImageIcon className='size-4' />
               </Button>
             </Hint>
