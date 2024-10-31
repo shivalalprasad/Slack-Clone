c83766647e60b16c692410895dc05401d3bdb9d9
Shivalal <shivalalprasad04@gmail.com>
2024-10-31 12:31:39 +0530

    image upload func ✔️

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -7,12 +7,20 @@ import { toast } from 'sonner'
 import { useCreateMessage } from '@/features/messages/api/use-create-message'
 import { useChannelId } from '@/hooks/use-channel-id'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
+import { useGenerateUploadUrl } from '@/features/upload/api/use-generate-upload-url'
+import { Id } from '../../../../../../convex/_generated/dataModel'
 
 const Editor = dynamic(() => import('@/components/editor'), {
   ssr: false
 })
 interface ChatInputProps {
   placeholder: string
+}
+type CreateMessageValues = {
+  channelId: Id<'channels'>
+  workspaceId: Id<'workspaces'>
+  body: string
+  image?: Id<'_storage'>
 }
 export default function ChatInput({ placeholder }: ChatInputProps) {
   const [editorKey, setEditorKey] = useState(0)
@@ -22,23 +30,41 @@ export default function ChatInput({ placeholder }: ChatInputProps) {
   const workspaceId = useWorkspaceId()
   const channelId = useChannelId()
   const { mutate: createMessage } = useCreateMessage()
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl()
   const handleSubmit = async ({ body, image }: { body: string; image: File | null }) => {
     console.log({ body, image })
     try {
       setIsPending(true)
-      createMessage(
-        {
-          workspaceId,
-          channelId,
-          body
-        },
-        { throwErrors: true }
-      )
+      editorRef.current?.enable(false)
+      const values: CreateMessageValues = {
+        channelId,
+        workspaceId,
+        body,
+        image: undefined
+      }
+      if (image) {
+        const url = await generateUploadUrl({}, { throwErrors: true })
+        if (!url) {
+          throw new Error('Url not found')
+        }
+        const result = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': image.type },
+          body: image
+        })
+        if (!result.ok) {
+          throw new Error('Failed to upload image')
+        }
+        const { storageId } = await result.json()
+        values.image = storageId
+      }
+      createMessage(values, { throwErrors: true })
       setEditorKey((prev) => prev + 1)
     } catch (error) {
       toast.error('Failed to send message')
     } finally {
       setIsPending(false)
+      editorRef.current?.enable(true)
     }
   }
 
---
+++ src/features/upload/api/use-generate-upload-url.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useCallback, useMemo, useState } from 'react'
+import { useMutation } from 'convex/react'
+import { api } from '../../../../convex/_generated/api'
+type ResponseType = string | null
+type Options = {
+  onSuccess?: (data: ResponseType) => void
+  onError?: (error: Error) => void
+  onSettled?: () => void
+  throwErrors?: boolean
+}
+export const useGenerateUploadUrl = () => {
+  const [data, setData] = useState<ResponseType>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const [status, setStatus] = useState<'success' | 'error' | 'settled' | 'pending' | null>(null)
+  const isPending = useMemo(() => status === 'pending', [status])
+  const isSuccess = useMemo(() => status === 'success', [status])
+  const isError = useMemo(() => status === 'error', [status])
+  const isSettled = useMemo(() => status === 'settled', [status])
+  const mutation = useMutation(api.upload.generateUploadUrl)
+  const mutate = useCallback(
+    async (values: {}, options?: Options) => {
+      try {
+        setData(null)
+        setError(null)
+        setStatus('pending')
+        const response = await mutation()
+        options?.onSuccess?.(response)
+        return response
+      } catch (error) {
+        setStatus('error')
+        setError(error as Error)
+        options?.onError?.(error as Error)
+        if (options?.throwErrors) {
+          throw error
+        }
+      } finally {
+        setStatus('settled')
+        options?.onSettled?.()
+      }
+    },
+    [mutation]
+  )
+  return {
+    mutate,
+    data,
+    error,
+    isPending,
+    isSuccess,
+    isError,
+    isSettled
+  }
+}
