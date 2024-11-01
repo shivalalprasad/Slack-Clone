c7ffceb4456a606dc64764c12857de115f90dde9
Shivalal <shivalalprasad04@gmail.com>
2024-11-02 02:52:52 +0530

    load more ♾️

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react'
+import { Loader } from 'lucide-react'
 import { differenceInMinutes, format, isToday, isYesterday } from 'date-fns'
+
 import { GetMessagesReturnType } from '@/features/messages/api/use-get-message'
 import { useCurrentMember } from '@/features/members/api/use-current-member'
+
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
+
 import { Message } from './message'
 import ChannelHero from './channel-hero'
 import { Id } from '../../convex/_generated/dataModel'
-import { useState } from 'react'
+
 const TIME_THRESHOLD = 5
+
 interface MessageListProps {
   memberName?: string
   memberImage?: string
@@ -19,12 +25,14 @@ interface MessageListProps {
   isLoadingMore: boolean
   canLoadMore: boolean
 }
+
 const formatDateLabel = (dateStr: string) => {
   const date = new Date(dateStr)
   if (isToday(date)) return 'Today'
   if (isYesterday(date)) return 'Yesterday'
   return format(date, 'EEEE, MMMM')
 }
+
 export function MessageList({
   memberName,
   memberImage,
@@ -37,8 +45,11 @@ export function MessageList({
   canLoadMore
 }: MessageListProps) {
   const [editingId, setEditingId] = useState<Id<'messages'> | null>(null)
+
   const workspaceId = useWorkspaceId()
+
   const { data: currentMember } = useCurrentMember({ workspaceId })
+
   const groupedMessages = data?.reduce((groups, message) => {
     const date = new Date(message._creationTime)
     const dateKey = format(date, 'yyyy-MM-dd')
@@ -46,8 +57,10 @@ export function MessageList({
       groups[dateKey] = []
     }
     groups[dateKey].unshift(message)
+
     return groups
   }, {} as Record<string, typeof data>)
+
   return (
     <div className='flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar'>
       {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
@@ -64,6 +77,7 @@ export function MessageList({
               prevMessage &&
               prevMessage.user?._id === message.user?._id &&
               differenceInMinutes(new Date(message._creationTime), new Date(prevMessage._creationTime)) < TIME_THRESHOLD
+
             return (
               <Message
                 key={message._id}
@@ -89,7 +103,32 @@ export function MessageList({
           })}
         </div>
       ))}
-       {variant === 'channel' && channelName && channelCreationTime && (
+      <div
+        className='h-1'
+        ref={(el) => {
+          const observer = new IntersectionObserver(
+            ([entry]) => {
+              if (entry.isIntersecting && canLoadMore) {
+                loadMore()
+              }
+            },
+            { threshold: 1.0 }
+          )
+
+          observer.observe(el)
+
+          return () => observer.disconnect()
+        }}
+      />
+      {isLoadingMore && (
+        <div className='text-center my-2 relative'>
+          <hr className='absolute top-1/2 left-0 right-0 border-t border-gray-300' />
+          <span className='relative inline-block bg-white px-4 py-1 rounded-full text-xs border-gray-300 shadow-sm'>
+            <Loader className='size-4 animate-spin' />
+          </span>
+        </div>
+      )}
+      {variant === 'channel' && channelName && channelCreationTime && (
         <ChannelHero name={channelName} creationTime={channelCreationTime} />
       )}
     </div>
